da827866e0e4ba6d89ad6cebf77ea9c519afdb5c
Jacques Marcoux <29962430+jacmarcx@users.noreply.github.com>
2020-10-01 01:34:34 -0500

    fix dates

--- river_bot.py
+++ river_bot.py
@@ -1,31 +1,35 @@
 import pandas as pd
 import twitter
 import os
+import datetime
 from os import environ
 import time
 
+while True:
+    # get data
+    url = 'https://data.winnipeg.ca/resource/q8w3-jhjb.csv'
 
-# get data
-url = 'https://data.winnipeg.ca/resource/q8w3-jhjb.csv'
+    data = pd.read_csv(url)
+    first_date = data.iloc[0]['james_feet']
+    date = pd.to_datetime(data.iloc[0]['reading_date']).strftime('%B %d, %Y')
+    hour = pd.to_datetime(data.iloc[0]['reading_date']).strftime('%H:%M')
+    next_update = datetime.datetime.now() + datetime.timedelta(hours=2)
+    next_update = next_update.strftime('%#I:%M %p')
+        
+    # send tweet
+    # api details
+    consumer_key = environ['CONSUMER_KEY']
+    consumer_secret = environ['CONSUMER_SECRET']
+    access_token = environ['ACCESS_TOKEN']
+    access_secret = environ['ACCESS_SECRET']
+    
 
-data = pd.read_csv(url)
-first_date = data.iloc[0]['james_feet']
-date = pd.to_datetime(data.iloc[0]['reading_date']).strftime('%B %d, %Y')
-hour = pd.to_datetime(data.iloc[0]['reading_date']).strftime('%H:%M')
+    t = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_secret)
 
-# send tweet
-#api details
-consumer_key = environ['CONSUMER_KEY']
-consumer_secret = environ['CONSUMER_SECRET']
-access_token = environ['ACCESS_TOKEN']
-access_secret = environ['ACCESS_SECRET']
+    t.PostUpdate(status=('Bartley Kives says:\n"The Red River level for ' + date +
+                         ' is: ' + str(first_date) + ' feet James. Next update at: ' + str(next_update) + '.'))
 
-t = twitter.Api(consumer_key=consumer_key,
-                  consumer_secret=consumer_secret,
-                  access_token_key=access_token,
-                  access_token_secret=access_secret)
-
-t.PostUpdate(status=('.@bkives says:\n"The Red River level for ' + date +
-                     ' is currently: ' + str(first_date) + ' feet James as of ' + str(hour)))
-
-time.sleep(150)
+    time.sleep(150)
