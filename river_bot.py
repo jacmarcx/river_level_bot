8d8d6884b915bfed0e445e94a2212b779bf081d6
Jacques Marcoux <29962430+jacmarcx@users.noreply.github.com>
2020-10-02 00:00:34 -0500

    remove comments block

--- river_bot.py
+++ river_bot.py
@@ -21,12 +21,12 @@ while True:
     next_update = datetime.datetime.now() + datetime.timedelta(hours=3)
     next_update = next_update.strftime('%#I:%M %p')
         
-    #send tweet
-    #api details
-    # consumer_key = environ['CONSUMER_KEY']
-    # consumer_secret = environ['CONSUMER_SECRET']
-    # access_token = environ['ACCESS_TOKEN']
-    # access_secret = environ['ACCESS_SECRET']
+    # send tweet
+    # api details
+    consumer_key = environ['CONSUMER_KEY']
+    consumer_secret = environ['CONSUMER_SECRET']
+    access_token = environ['ACCESS_TOKEN']
+    access_secret = environ['ACCESS_SECRET']
     
 
     #charting data
@@ -39,14 +39,14 @@ while True:
     plt.tight_layout()        
     plt.savefig('chart.png', dpi=60)
    
-    # t = twitter.Api(consumer_key=consumer_key,
-                      # consumer_secret=consumer_secret,
-                      # access_token_key=access_token,
-                      # access_token_secret=access_secret)
+    t = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_secret)
 
-    # t.PostUpdate(status=('The Red River level for ' + date +
-                         # ' is: ' + str(first_value) + ' feet James.\nNext update at: ' + str(next_update) + '.'),
-                         # media='chart.png')
+    t.PostUpdate(status=('The Red River level for ' + date +
+                         ' is: ' + str(first_value) + ' feet James.\nNext update at: ' + str(next_update) + '.'),
+                         media='chart.png')
 
     #time.sleep(10800)
     time.sleep(180)
